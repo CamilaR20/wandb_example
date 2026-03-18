fbdc09152e8b8fbc20132d31a1d33a19b9178bb4
Camila R <macaroa123@gmail.com>
2026-03-18 13:19:04 -0500

    Added return of embeddings.

--- src/wandb_example/models.py
+++ src/wandb_example/models.py
@@ -4,40 +4,38 @@ import torch.nn as nn
 class MLP(nn.Module):
     def __init__(self, image_size, n_classes):
         super().__init__()
-        self.net = nn.Sequential(
+        self.mlp = nn.Sequential(
             nn.Flatten(),
             nn.Linear(image_size[0] * image_size[1], 128),
             nn.ReLU(),
-            nn.Linear(128, n_classes),
         )
+        self.fc = nn.Linear(128, n_classes)
 
     def forward(self, x):
-        return self.net(x)
+        h = self.mlp(x)
+        y = self.fc(h)
+        return h, y
 
 
 class CNN(nn.Module):
     def __init__(self, n_classes=2, dropout=0.5):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.maxpool = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.bn2 = nn.BatchNorm2d(64)
+        self.conv_net = nn.Sequential(
+            nn.Conv2d(1, 32, 3, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 3, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+        )
         self.dropout = nn.Dropout(dropout)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(64, n_classes)
 
-        self.relu = nn.ReLU(inplace=True)
-
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        x = self.avgpool(x).flatten(1)
-        x = self.dropout(x)
-        x = self.fc(x)
-        return x
+        h = self.conv_net(x)
+        h_drop = self.dropout(h)
+        y = self.fc(h_drop)
+        return h, y
